232728c9e50b7e2860a9f29138b3b3894eb502cf
Anna Shilova <shilovanna@gmail.com>
2016-01-25 13:03:02 -0800

    adding a descriptive version of asyncIO.js

--- asyncIO.js
+++ asyncIO.js
@@ -1,12 +1,14 @@
-//does the order of the functions matter? 
+/*use the non-blocking async model to read and split it 
+based on endline character */
 
+//wrapper for file system: both synchronous and asynchronous methods
 var fs = require('fs');
 
+//callback function
 function finishedReading(error, fileContent) {
 	if(error) return console.error(error);
 
 	console.log(fileContent.split('\n').length - 1); 
-
 }
 
 fs.readFile(process.argv[2], 'utf8', finishedReading); 
