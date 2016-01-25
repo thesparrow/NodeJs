144c0d4e47257639ccb6e0a58bc14158e4e68e24
Anna Shilova <shilovanna@gmail.com>
2016-01-25 14:23:45 -0800

    improving read file synchornously

--- firstIO.js
+++ firstIO.js
@@ -1,14 +1,6 @@
+/*Read content of the file synchronously, and parse based on newline*/
 
 var fs = require('fs'); 
-
-//var buff = fs.readFileSync(process.argv[2]);
-//console.log(process.argv[2]); 
-//var fileString = buff.toString(); 
-//var numberOfNewlines = fileString.split('\n');
-
-//console.log(numberOfNewlines.length); 
-
-//console.log(numberOfNewlines.length - 1);
 
 console.log(fs.readFileSync(process.argv[2],'utf8').split('\n').length - 1); 
 
